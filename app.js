0e5c6468bc218a8447b6e69255ca7efcedd478c2
Bora Son <btson310@gmail.com>
2019-07-18 17:21:08 -0600

    styled colored page

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 // jshint esversion: 6
-
+// connection variables
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 // const ejs = require('ejs');
 const port = 3000;
-const items = [];
 
-mongoose.connect('mongodb://localhost:27017/todolistDB');
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true
+});
+
+const itemSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model('item', itemSchema);
+const item1 = new Item({
+  name: 'Welcome to your to do list.'
+});
+const item2 = new Item({
+  name: 'Enter a task and hit the send button.'
+});
+const item3 = new Item({
+  name: 'Click on the garbage can to delete a task 👉'
+});
+
+const defaultItems = [item1, item2, item3];
+// Item.insertMany(defaultItems, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('Sucessfully added default items to DB');
+//   }
+// });
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
@@ -26,16 +52,39 @@ app.get('/', (req, res) => {
   };
   let date = today.toLocaleDateString('en-US', options);
 
-  res.render('list', {
-    newItems: items,
-    date: date
+  Item.find(function (err, foundItems) {
+    if (foundItems === 0) {
+      Item.insertMany(defaultItems, (req, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Sucessfully added default items to DB');
+        }
+      });
+    }
+    res.render('list', {
+      newItems: foundItems,
+      date: date
+    });
   });
+
+
+
 });
 
 app.post('/', (req, res) => {
   const newPost = req.body.newListItem;
-  console.log(newPost);
-  items.push(newPost);
+  let capitalizedPost = newPost.charAt(0).toUpperCase() + newPost.slice(1);
+  console.log(capitalizedPost);
+  Item.create({
+    name: capitalizedPost
+  }, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('post successfully added to DB');
+    }
+  });
   res.redirect('/');
 });
 
