ff55164919900e47e0c10dc7960a3253c72e553c
Bora Son <btson310@gmail.com>
2019-07-18 10:39:10 -0600

    set up list page

---
+++ app.js
@@ -0,0 +1,17 @@
+// jshint esversion: 6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+// const ejs = require('ejs');
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+
+  res.render('list');
+});
+
+
+app.listen(port, () => console.log(`Server has started on port ${port}.`));
